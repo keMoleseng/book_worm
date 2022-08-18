1be471ed9d8c6cae9ed94630765938124d0f4e30
keMoleseng <mmokgosi9@gmail.com>
2022-08-18 17:16:04 +0200

    Adding reusable Table component

---
+++ src/components/useTable.js
@@ -0,0 +1,13 @@
+import { Table } from "@mui/material"
+
+export default function useTable(records, headCells) {
+    const TblContainer = props => (
+        <Table>
+
+        </Table>
+    )
+
+    return{
+        TblContainer
+    }
+}
