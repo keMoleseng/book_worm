422fce2e808c15e906d9c47000e63be339c04eec
keMoleseng <mmokgosi9@gmail.com>
2022-08-19 08:21:04 +0200

    Adding pagination

--- src/components/useTable.js
+++ src/components/useTable.js
@@ -1,13 +1,81 @@
-import { Table } from "@mui/material"
+import { Table, TableCell, TableHead, TablePagination, TableRow, TableSortLabel } from "@mui/material"
+import { useState } from "react";
 
 export default function useTable(records, headCells) {
-    const TblContainer = props => (
-        <Table>
+    const pages = [5, 10, 25];
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(pages[page]);
+    const [order, setOrder] = useState();
+    const [orderby, setOrderBy] = useState();
 
+    const TblContainer = props => (
+        <Table size='small'
+            sx={{
+                '& thead th': {
+                    fontWeight: '600',
+                    color: 'darkOlive',
+                    backgroundColor: 'beige'
+                },
+                '& tbody td': {
+                    fontWeight: '300',
+                    fontSize: '0.85em'
+                },
+                '& tbody tr:hover': {
+                    backgroundColor: '#fffbf2',
+                    cursor: 'pointer'
+                }
+            }}
+        >
+            {props.children}
         </Table>
     )
+    
+    const TblHead = props => {
+        return (
+            <TableHead>
+                <TableRow>
+                    {
+                        headCells.map(headCell => (<TableCell key={headCell.id}>
+                            <TableSortLabel>
+                                {headCell.label}
+                            </TableSortLabel>
+                        </TableCell>))
+                    }
+                </TableRow>
+            </TableHead>
+    )
+    }
 
+    const TblPagination = () => {
+        const handleChangePage = (e, newPage) => {
+            setPage(newPage)
+        }
+    
+        const handleChangeRowsPerPage = e => {
+            setRowsPerPage(parseInt(e.target.value, 10));
+            setPage(0)
+        }
+
+        return (
+        <TablePagination
+            component='div'
+            count={records.length}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPageOptions={pages}
+            rowsPerPage={rowsPerPage} 
+            onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+    )
+}
+
+    const recordsAfterPagingAndSorting = () => {
+        return records.slice(page*rowsPerPage, (page+1)*rowsPerPage)
+    }
     return{
-        TblContainer
+        TblContainer,
+        TblHead,
+        TblPagination,
+        recordsAfterPagingAndSorting
     }
 }
--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,9 +1,28 @@
 import PageHeader from '../components/PageHeader';
 import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import { Box, Paper } from '@mui/material';
+import { Box, ListItem, Paper, TableBody, TableCell, TableRow } from '@mui/material';
 import BookForm from '../pages/BookForm';
+import useTable from '../components/useTable';
+import * as booksServices from '../services/book.service'
+import { useState } from 'react';
+
+const headCells = [
+    { id: 'title', label: 'Title' }, 
+    { id: 'author', label: 'Author' }, 
+    { id: 'publisher', label: 'Publisher' },
+    { id: 'genre', label: 'Genre' },
+    { id: 'year', label: 'Year' }  
+]
 
 export default function Books() {
+    const [records, setRecords] = useState(booksServices.getAllBooks())
+    const {
+        TblContainer,
+        TblHead,
+        TblPagination,
+        recordsAfterPagingAndSorting
+    } = useTable(records, headCells);
+
     return(   
             <Box component="div" 
                 sx={{
@@ -14,8 +33,8 @@ export default function Books() {
                 }}  
             >
                 <PageHeader 
-                    title="Heading"
-                    subtitle="Sub-heading"
+                    title="Library"
+                    subtitle="Your current book collection"
                     icon={<LibraryBooksIcon />}
                 /> 
                 <Paper
@@ -24,7 +43,24 @@ export default function Books() {
                         padding: (theme) => theme.spacing(3)
                     }}
                 >
-                    <BookForm /> 
+                    <TblContainer>
+                        <TblHead />
+                        <TableBody >
+                            {recordsAfterPagingAndSorting().map(record => (
+                                <TableRow key={record.id} >
+                                    <TableCell >{record.title} </TableCell>
+                                    <TableCell >{record.author} </TableCell>
+                                    <TableCell >{record.publisher} </TableCell>
+                                    <TableCell >{record.genre} </TableCell>
+                                    <TableCell >{record.year} </TableCell>
+                                    {/* <TableCell>{record.pageNo} </TableCell> */}
+                                   
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </TblContainer>
+                    {/* <BookForm />  */}
+                    <TblPagination />
                 </Paper> 
             </Box>     
     )
