6b11ff4a42e4d29f591aea622dd31c4192d03d17
keMoleseng <mmokgosi9@gmail.com>
2022-08-19 22:31:13 +0200

    Adding reusable notifications component

---
+++ src/components/Notification.js
@@ -0,0 +1,30 @@
+import { Alert, Snackbar } from "@mui/material";
+
+export default function Notification(props) {
+        const { notify, setNotify } = props;
+
+        const handleClose = (e, reason) => {
+
+            setNotify({
+                ...notify,
+                isOpen: false
+            })
+        }
+        console.log(notify)
+    return(
+        <Snackbar
+            open={notify.isOpen}
+            autoHideDuration={10000}
+            anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+            sx={{
+                marginTop: theme => theme.spacing(5.5)
+            }}
+            onClose={handleClose}
+        >
+            <Alert severity={notify.type}
+            onClose={handleClose}>
+                {notify.message}
+            </Alert>
+        </Snackbar>
+    )
+}
