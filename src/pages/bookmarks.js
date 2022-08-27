03d35052050be7dca158367232154b6cd05ca5e8
keMoleseng <mmokgosi9@gmail.com>
2022-08-27 13:03:50 +0200

    building bookmarks page

--- src/pages/bookmarks.js
+++ src/pages/bookmarks.js
@@ -24,7 +24,6 @@ export default function Bookmarks(props) {
     const {
         TblContainer,
         TblHead,
-        TblPagination,
         recordsAfterPagingAndSorting
     } = useTable(records, headCells, filterFn);
 
