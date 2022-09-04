fc41db3caa9d11422120c1560d3b9062fa20a904
keMoleseng <mmokgosi9@gmail.com>
2022-09-04 09:43:42 +0200

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -64,7 +64,7 @@ export default function Header() {
                                     />
                                 </Badge>
                             </IconButton>
-                            <IconButton onClick={() => navigate('/login')}>
+                            <IconButton onClick={() => navigate('/')}>
                                 <LogoutIcon />
                             </IconButton>
                         </Grid>
